a0495aa38b9639f23f2cdffa1fdd5984a5d290f6
MrNOOB <56033162+MrNOOB07@users.noreply.github.com>
2019-11-07 02:18:57 +0700

    Update login.py

--- login.py
+++ login.py
@@ -8,3 +8,23 @@ if sandi == 'NOOB07' and user == 'Mr':
 else:
 
     print ("Username Atau Password Salah!")
+
+import os
+import sys
+import time
+def waktu (s):
+ for c in s + '\n':
+  sys.stdout.write(c)
+  sys.stdout.flush()
+  time.sleep(1./10)
+os.system('clear')
+
+waktu ('\033[1;36m Hallo Saya Mr.NOOB07 -> :v INGIN MEMBAGIKAN TOOLS/SCRIPT KEPADA KALIAN')
+waktu ('\033[1;36m TAPI,MOHON DI MAAFKAN JIKA TOOLS NYA SEDIKIT Hargai Saya Yah')
+os.system('clear')
+waktu ('\033[1;36m Semoga Tools Nya Bermanfaat Buat Kalian :v')
+
+print ("=====================================")
+print ("Author : Mr.NOOB07")
+print ("Team : C1ONING CYBER ARMY")
+print ("Thanks To : B0C1L Gans") ("=====================================")
